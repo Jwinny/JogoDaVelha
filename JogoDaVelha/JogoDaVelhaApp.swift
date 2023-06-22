5115f2ed27e407ea5a725f313460d6a24c83114d
José Winny <jose.winny@icloud.com>
2023-06-22 20:25:17 -0300

    Initial Commit

---
+++ JogoDaVelha/JogoDaVelhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogoDaVelhaApp.swift
+//  JogoDaVelha
+//
+//  Created by José Winny on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct JogoDaVelhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
